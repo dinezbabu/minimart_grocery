1a0107c3600a491e3f0fdec9fddcdee89693f37a
dgowda <Dealer30!dg>
2022-01-16 01:00:37 +0530

    Working UI

--- lib/config.dart
+++ lib/config.dart
@@ -1,6 +1,6 @@
 class Config{
   static const String appName="MiniMart App";
-  static const String apiURL="";
+  static const String apiURL="192.168.0.102:4000";
   static const String loginAPI="/users/login";
   static const String registerAPI="/users/register";
   static const String userProfileAPI="/users/user-profile";
